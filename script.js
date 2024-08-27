c230cf0c092e204a4876194b021e4b86010fed7d
Fernando Carvalho <84986212+Fer96carvalho@users.noreply.github.com>
2024-08-27 14:53:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-digite o seu script aqui
+    window.onload = function() {
+      var script = document.createElement('script');
+      script.src = "https://vlibras.gov.br/app/vlibras-plugin.js";
+      script.onload = function() {
+          new window.VLibras.Widget('https://vlibras.gov.br/app');
+      };
+      document.head.appendChild(script);
+  };
