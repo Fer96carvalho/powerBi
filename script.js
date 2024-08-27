0d854562755c3f2bd76968339a9c16943e4c9dfd
Fernando Carvalho <84986212+Fer96carvalho@users.noreply.github.com>
2024-08-27 15:01:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-    window.onload = function() {
-      var script = document.createElement('script');
-      script.src = "https://vlibras.gov.br/app/vlibras-plugin.js";
-      script.onload = function() {
-          new window.VLibras.Widget('https://vlibras.gov.br/app');
-      };
-      document.head.appendChild(script);
-  };
+window.onload = function() {
+    var script = document.createElement('script');
+    script.src = "https://vlibras.gov.br/app/vlibras-plugin.js";
+    var script2 = document.createElement('script');
+
+    script2.onload = function() {
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    };
+    document.head.appendChild(script);
+    document.head.appendChild(script2);
+
+};
